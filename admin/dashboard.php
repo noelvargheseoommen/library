1ff4c772a97b06be2162162a56e1861274f9b2b4
nikhilputhumana <nikhilp.csb1923@saintgits.org>
2022-02-23 22:12:17 +0530

    Changes made to admin pages

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -7,226 +7,145 @@ if(strlen($_SESSION['alogin'])==0)
 header('location:index.php');
 }
 else{?>
+
+
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html>
+
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
-    <!--[if IE]>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <![endif]-->
-    <title>Online Library Management System | Admin Dash Board</title>
-    <!-- BOOTSTRAP CORE STYLE  -->
-    <link href="assets/css/bootstrap.css" rel="stylesheet" />
-    <!-- FONT AWESOME STYLE  -->
-    <link href="assets/css/font-awesome.css" rel="stylesheet" />
-    <!-- CUSTOM STYLE  -->
-    <link href="assets/css/style.css" rel="stylesheet" />
-    <!-- GOOGLE FONT -->
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
-
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Admin Dashboard</title>
+    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
+    <link rel="stylesheet" href="../assets/fonts/fontawesome-all.min.css">
+    <link rel="stylesheet" href="../assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="../assets/fonts/fontawesome5-overrides.min.css">
+    <link rel="stylesheet" href="../assets/css/untitled.css">
 </head>
-<body>
-      <!------MENU SECTION START-->
-<?php include('includes/header.php');
-	  include('bgwork.php');?>
-<!-- MENU SECTION END-->
-    <div class="content-wrapper">
-         <div class="container">
-        <div class="row pad-botm">
-            <div class="col-md-12">
-                <h4 class="header-line">ADMIN DASHBOARD</h4>
-                
+
+<body id="page-top">
+
+  <?php
+	include('bgwork.php');?>
+
+    <div id="wrapper">
+        <nav class="navbar navbar-dark align-items-start sidebar sidebar-dark accordion bg-gradient-primary p-0">
+            <div class="container-fluid d-flex flex-column p-0"><a class="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-0" href="#">
+                    <div class="sidebar-brand-icon rotate-n-15"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-book-half" style="font-size: 31px;">
+                            <path d="M8.5 2.687c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"></path>
+                        </svg></div>
+                    <div class="sidebar-brand-text mx-3"><span>SAINTGITS<br>library<br></span></div>
+                </a>
+                <hr class="sidebar-divider my-0">
+                <ul class="navbar-nav text-light" id="accordionSidebar">
+                    <li class="nav-item"><a class="nav-link" href="dashboard.php"><i class="fa fa-dashboard"></i><span>Dashboard</span></a></li>
+                    <li class="nav-item"><a class="nav-link" href="add-book.php"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none">
+                                <path d="M12 6C12.5523 6 13 6.44772 13 7V11H17C17.5523 11 18 11.4477 18 12C18 12.5523 17.5523 13 17 13H13V17C13 17.5523 12.5523 18 12 18C11.4477 18 11 17.5523 11 17V13H7C6.44772 13 6 12.5523 6 12C6 11.4477 6.44772 11 7 11H11V7C11 6.44772 11.4477 6 12 6Z" fill="currentColor"></path>
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M5 22C3.34315 22 2 20.6569 2 19V5C2 3.34315 3.34315 2 5 2H19C20.6569 2 22 3.34315 22 5V19C22 20.6569 20.6569 22 19 22H5ZM4 19C4 19.5523 4.44772 20 5 20H19C19.5523 20 20 19.5523 20 19V5C20 4.44772 19.5523 4 19 4H5C4.44772 4 4 4.44772 4 5V19Z" fill="currentColor"></path>
+                            </svg><span>Add books</span></a></li>
+                    <li class="nav-item"><a class="nav-link" href="requested.html"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icon-tabler-manual-gearbox">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                <circle cx="5" cy="6" r="2"></circle>
+                                <circle cx="12" cy="6" r="2"></circle>
+                                <circle cx="19" cy="6" r="2"></circle>
+                                <circle cx="5" cy="18" r="2"></circle>
+                                <circle cx="12" cy="18" r="2"></circle>
+                                <line x1="5" y1="8" x2="5" y2="16"></line>
+                                <line x1="12" y1="8" x2="12" y2="16"></line>
+                                <path d="M19 8v2a2 2 0 0 1 -2 2h-12"></path>
+                            </svg><span>Manage books requested</span></a></li>
+                    <li class="nav-item"><a class="nav-link" href="user-details.html"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-eye-fill">
+                                <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"></path>
+                                <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"></path>
+                            </svg>View user details</a></li>
+                </ul>
+                <div class="text-center d-none d-md-inline"><button class="btn rounded-circle border-0" id="sidebarToggle" type="button"></button></div>
+            </div>
+        </nav>
+        <div class="d-flex flex-column" id="content-wrapper">
+            <div id="content">
+                <nav class="navbar navbar-light navbar-expand bg-white shadow mb-4 topbar static-top">
+                    <div class="container-fluid"><button class="btn btn-link d-md-none rounded-circle me-3" id="sidebarToggleTop" type="button"><i class="fas fa-bars"></i></button>
+                        <ul class="navbar-nav flex-nowrap ms-auto">
+                            <li class="nav-item dropdown show d-sm-none no-arrow"><a class="dropdown-toggle nav-link" aria-expanded="true" data-bs-toggle="dropdown" href="#"><i class="fas fa-search"></i></a>
+                                <div class="dropdown-menu show dropdown-menu-end p-3 animated--grow-in" aria-labelledby="searchDropdown">
+                                    <form class="me-auto navbar-search w-100">
+                                        <div class="input-group"><input class="bg-light form-control border-0 small" type="text" placeholder="Search for ...">
+                                            <div class="input-group-append"><button class="btn btn-primary py-0" type="button"><i class="fas fa-search"></i></button></div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </li>
+                            <li class="nav-item dropdown no-arrow">
+                                <div class="nav-item dropdown no-arrow"><a class="dropdown-toggle nav-link" aria-expanded="true" data-bs-toggle="dropdown" href="#"><span class="d-none d-lg-inline me-2 text-gray-600 small">Admin</span><img class="border rounded-circle img-profile" src="../assets/img/avatars/avatar3.jpeg"></a>
+                                    <div class="dropdown-menu shadow dropdown-menu-end animated--grow-in"><a class="dropdown-item" href="../Pages/user-login.html"><i class="fas fa-sign-out-alt fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Logout</a></div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+                <div class="container-fluid">
+                    <div class="d-sm-flex justify-content-between align-items-center mb-4">
+                        <h3 class="text-dark mb-0" style="font-size: 30.752px;">Dashboard</h3>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6 col-xl-3 mb-4" style="width: 50%;">
+                            <div class="card shadow border-start-primary py-2">
+                                <div class="card-body">
+                                    <div class="row align-items-center no-gutters">
+                                        <div class="col me-2">
+
+                                        <?php 
+                                          $sql ="SELECT id from tblbooks ";
+                                          $query = $dbh -> prepare($sql);
+                                          $query->execute();
+                                          $results=$query->fetchAll(PDO::FETCH_OBJ);
+                                          $listdbooks=$query->rowCount();
+                                        ?>
+                            
+                                          <div class="text-uppercase text-primary fw-bold text-xs mb-1"><span style="font-size: 17.2px;">BOOKS LISTED</span></div>
+                                          <div class="text-dark fw-bold h5 mb-0" style="font-size: 27px;"><span> <?php echo htmlentities($listdbooks);?> </span></div>
+                                        </div>
+                                        <div class="col-auto"><i class="fas fa-book fa-2x text-gray-300"></i></div>
+                                    </div>
+                                </div>
                             </div>
+                        </div>
+                        <div class="col-md-6 col-xl-3 mb-4" style="width: 50%;">
+                            <div class="card shadow border-start-primary py-2">
+                                <div class="card-body">
+                                    <div class="row align-items-center no-gutters">
+                                        <div class="col me-2">
 
-        </div>
-             
-             <div class="row">
+                                        <?php 
+                                          $sql3 ="SELECT id from tblstudents ";
+                                          $query3 = $dbh -> prepare($sql3);
+                                          $query3->execute();
+                                          $results3=$query3->fetchAll(PDO::FETCH_OBJ);
+                                          $regstds=$query3->rowCount();
+                                        ?>
 
- <div class="col-md-3 col-sm-3 col-xs-6">
-                      <div class="alert alert-success back-widget-set text-center">
-                            <i class="fa fa-book fa-5x"></i>
-<?php 
-$sql ="SELECT id from tblbooks ";
-$query = $dbh -> prepare($sql);
-$query->execute();
-$results=$query->fetchAll(PDO::FETCH_OBJ);
-$listdbooks=$query->rowCount();
-?>
-
-
-                            <h3><?php echo htmlentities($listdbooks);?></h3>
-                      Books Listed
+                                            <div class="text-uppercase text-primary fw-bold text-xs mb-1"><span style="font-size: 17.2px;">Registered users</span></div>
+                                            <div class="text-dark fw-bold h5 mb-0" style="font-size: 27px;"><span> <?php echo htmlentities($regstds);?> </span></div>
+                                        </div>
+                                        <div class="col-auto"><i class="fas fa-user-friends fa-2x text-gray-300"></i></div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-
-            
-                 <div class="col-md-3 col-sm-3 col-xs-6">
-                      <div class="alert alert-info back-widget-set text-center">
-                            <i class="fa fa-bars fa-5x"></i>
-<?php 
-$sql1 ="SELECT id from tblissuedbookdetails ";
-$query1 = $dbh -> prepare($sql1);
-$query1->execute();
-$results1=$query1->fetchAll(PDO::FETCH_OBJ);
-$issuedbooks=$query1->rowCount();
-?>
-
-                            <h3><?php echo htmlentities($issuedbooks);?> </h3>
-                           Times Book Issued
-                        </div>
-                    </div>
-             
-               <div class="col-md-3 col-sm-3 col-xs-6">
-                      <div class="alert alert-warning back-widget-set text-center">
-                            <i class="fa fa-recycle fa-5x"></i>
-<?php 
-$status=1;
-$sql2 ="SELECT id from tblissuedbookdetails where ReturnStatus=:status";
-$query2 = $dbh -> prepare($sql2);
-$query2->bindParam(':status',$status,PDO::PARAM_STR);
-$query2->execute();
-$results2=$query2->fetchAll(PDO::FETCH_OBJ);
-$returnedbooks=$query2->rowCount();
-?>
-
-                            <h3><?php echo htmlentities($returnedbooks);?></h3>
-                          Times  Books Returned
-                        </div>
-                    </div>
-               <div class="col-md-3 col-sm-3 col-xs-6">
-                      <div class="alert alert-danger back-widget-set text-center">
-                            <i class="fa fa-users fa-5x"></i>
-                            <?php 
-$sql3 ="SELECT id from tblstudents ";
-$query3 = $dbh -> prepare($sql3);
-$query3->execute();
-$results3=$query3->fetchAll(PDO::FETCH_OBJ);
-$regstds=$query3->rowCount();
-?>
-                            <h3><?php echo htmlentities($regstds);?></h3>
-                           Registered Users
-                        </div>
-                    </div>
-
-        </div>
-
-
-
- <div class="row">
-
- <div class="col-md-3 col-sm-3 col-xs-6">
-                      <div class="alert alert-success back-widget-set text-center">
-                            <i class="fa fa-user fa-5x"></i>
-<?php 
-$sql4 ="SELECT id from tblauthors ";
-$query4 = $dbh -> prepare($sql4);
-$query4->execute();
-$results4=$query4->fetchAll(PDO::FETCH_OBJ);
-$listdathrs=$query4->rowCount();
-?>
-
-
-                            <h3><?php echo htmlentities($listdathrs);?></h3>
-                      Publications Listed
-                        </div>
-                    </div>
-
-            
-                 <div class="col-md-3 col-sm-3 rscol-xs-6">
-                      <div class="alert alert-info back-widget-set text-center">
-                            <i class="fa fa-file-archive-o fa-5x"></i>
-<?php 
-$sql5 ="SELECT id from tblcategory ";
-$query5 = $dbh -> prepare($sql5);
-$query5->execute();
-$results5=$query5->fetchAll(PDO::FETCH_OBJ);
-$listdcats=$query5->rowCount();
-?>
-
-                            <h3><?php echo htmlentities($listdcats);?> </h3>
-                           Listed Categories
-                        </div>
-                    </div>   
-<div class="col-md-3 col-sm-3 rscol-xs-6">
-                      <div class="alert alert-info back-widget-set text-center">
-                            <i class="fa fa-money fa-5x"></i>
-<?php 
-$ret="select * from tblfine where 1";
-$query= $dbh -> prepare($ret);
-$query-> execute();
-$results=$query->fetchAll(PDO::FETCH_OBJ);
-if($query->rowCount() > 0)
-{
-foreach($results as $result)
-{
-$fine=$result->fine;	
-}
-}
-?>
-
-                            <h3><?php echo htmlentities($fine);?> </h3>
-                           Current Fine Per Day
-                        </div>
-                    </div>     
-					
-
-        </div> 
-
-             <div class="row">
-
-              <div class="col-md-10 col-sm-8 col-xs-12 col-md-offset-1">
-                    <div id="carousel-example" class="carousel slide slide-bdr" data-ride="carousel" >
-                   
-                    <div class="carousel-inner">
-                        <div class="item active">
-
-                            <img src="assets/img/1.jpg" alt="" />
-                           
-                        </div>
-                        <div class="item">
-                            <img src="assets/img/2.jpg" alt="" />
-                          
-                        </div>
-                        <div class="item">
-                            <img src="assets/img/3.jpg" alt="" />
-                           
-                        </div>
-                    </div>
-                    <!--INDICATORS-->
-                     <ol class="carousel-indicators">
-                        <li data-target="#carousel-example" data-slide-to="0" class="active"></li>
-                        <li data-target="#carousel-example" data-slide-to="1"></li>
-                        <li data-target="#carousel-example" data-slide-to="2"></li>
-                    </ol>
-                    <!--PREVIUS-NEXT BUTTONS-->
-                     <a class="left carousel-control" href="#carousel-example" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left"></span>
-  </a>
-  <a class="right carousel-control" href="#carousel-example" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right"></span>
-  </a>
                 </div>
-              </div>
-                 
-               
-             
-               
-            
-             </div>
-            
+            </div>
+            <footer class="bg-white sticky-footer"></footer>
+        </div><a class="border rounded d-inline scroll-to-top" href="#page-top"><i class="fas fa-angle-up"></i></a>
     </div>
-    </div>
-     <!-- CONTENT-WRAPPER SECTION END-->
-    <!-- JAVASCRIPT FILES PLACED AT THE BOTTOM TO REDUCE THE LOADING TIME  -->
-    <!-- CORE JQUERY  -->
-    <script src="assets/js/jquery-1.10.2.js"></script>
-    <!-- BOOTSTRAP SCRIPTS  -->
-    <script src="assets/js/bootstrap.js"></script>
+    <script src="../assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="../assets/js/theme.js"></script>
       <!-- CUSTOM SCRIPTS  -->
     <script src="assets/js/custom.js"></script>
 </body>
+
 </html>
+
 <?php } ?>
