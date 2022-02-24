3eaa9bb9f8a62afb5c59315dd5399e25d9cdc201
nikhilputhumana <nikhilp.csb1923@saintgits.org>
2022-02-24 07:45:56 +0530

    profle changed

--- index.php
+++ index.php
@@ -2,27 +2,43 @@
 session_start();
 error_reporting(0);
 include('includes/config.php');
-if($_SESSION['alogin']!=''){
-$_SESSION['alogin']='';
+if($_SESSION['login']!=''){
+$_SESSION['login']='';
 }
 if(isset($_POST['login']))
 {
-$username=$_POST['username'];
+
+$email=$_POST['emailid'];
 $password=md5($_POST['password']);
-$sql ="SELECT UserName,Password FROM admin WHERE UserName=:username and Password=:password";
+$sql ="SELECT FullName,EmailId,Password,StudentId,Status FROM tblstudents WHERE EmailId=:email and Password=:password";
 $query= $dbh -> prepare($sql);
-$query-> bindParam(':username', $username, PDO::PARAM_STR);
+$query-> bindParam(':email', $email, PDO::PARAM_STR);
 $query-> bindParam(':password', $password, PDO::PARAM_STR);
 $query-> execute();
 $results=$query->fetchAll(PDO::FETCH_OBJ);
+
 if($query->rowCount() > 0)
 {
-$_SESSION['alogin']=$_POST['username'];
-echo "<script type='text/javascript'> document.location ='admin/dashboard.php'; </script>";
-} else{
+ foreach ($results as $result) {
+ $_SESSION['stdid']=$result->StudentId;
+ $_SESSION['username']=$result->FullName;
+if($result->Status==1)
+{
+$_SESSION['login']=$_POST['emailid'];
+echo "<script type='text/javascript'> document.location ='dashboard.php'; </script>";
+} else {
+echo "<script>alert('Your Account Has been blocked .Please contact admin');</script>";
+
+}
+}
+
+} 
+
+else{
 echo "<script>alert('Invalid Details');</script>";
 }
 }
+
 ?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -31,7 +47,7 @@ echo "<script>alert('Invalid Details');</script>";
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>Online Library Management System</title>
+    <title>Online Library Management System | </title>
     <!-- BOOTSTRAP CORE STYLE  -->
     <link href="assets/css/bootstrap.css" rel="stylesheet" />
     <!-- FONT AWESOME STYLE  -->
@@ -50,7 +66,7 @@ echo "<script>alert('Invalid Details');</script>";
 <div class="container">
 <div class="row pad-botm">
 <div class="col-md-12">
-<h4 class="header-line">ADMIN LOGIN FORM</h4>
+<h4 class="header-line">USER LOGIN FORM</h4>
 </div>
 </div>
              
@@ -65,15 +81,17 @@ echo "<script>alert('Invalid Details');</script>";
 <form role="form" method="post">
 
 <div class="form-group">
-<label>Enter Username</label>
-<input class="form-control" type="text" name="username" autocomplete="off" required />
+<label>Enter Email id</label>
+<input class="form-control" type="text" name="emailid" required autocomplete="off" />
 </div>
 <div class="form-group">
 <label>Password</label>
-<input class="form-control" type="password" name="password" autocomplete="off" required />
-</div> 
+<input class="form-control" type="password" name="password" required autocomplete="off"  />
+<p class="help-block"><a href="user-forgot-password.php">Forgot Password</a></p>
+</div>
 
- <button type="submit" name="login" class="btn btn-info">LOGIN </button>
+
+ <button type="submit" name="login" class="btn btn-info">LOGIN </button> | <a href="signup.php">Not Register Yet</a>
 </form>
  </div>
 </div>
@@ -90,6 +108,6 @@ echo "<script>alert('Invalid Details');</script>";
     <script src="assets/js/bootstrap.js"></script>
       <!-- CUSTOM SCRIPTS  -->
     <script src="assets/js/custom.js"></script>
-</script>
+
 </body>
 </html>
--- signup.php
+++ signup.php
@@ -3,42 +3,7 @@ session_start();
 include('includes/config.php');
 error_reporting(0);
 if(isset($_POST['signup']))
-{
-//code for captach verification
-if ($_POST["vercode"] != $_SESSION["vercode"] OR $_SESSION["vercode"]=='')  {
-      //Code for student ID
-$count_my_page = ("studentid.txt");
-$hits = file($count_my_page);
-$hits[0] ++;
-$fp = fopen($count_my_page , "w");
-fputs($fp , "$hits[0]");
-fclose($fp); 
-$StudentId= $hits[0];   
-$fname=$_POST['fullanme'];
-$mobileno=$_POST['mobileno'];
-$email=$_POST['email']; 
-$password=md5($_POST['password']); 
-$status=1;
-$sql="INSERT INTO  tblstudents(StudentId,FullName,MobileNumber,EmailId,Password,Status) VALUES(:StudentId,:fname,:mobileno,:email,:password,:status)";
-$query = $dbh->prepare($sql);
-$query->bindParam(':StudentId',$StudentId,PDO::PARAM_STR);
-$query->bindParam(':fname',$fname,PDO::PARAM_STR);
-$query->bindParam(':mobileno',$mobileno,PDO::PARAM_STR);
-$query->bindParam(':email',$email,PDO::PARAM_STR);
-$query->bindParam(':password',$password,PDO::PARAM_STR);
-$query->bindParam(':status',$status,PDO::PARAM_STR);
-$query->execute();
-$lastInsertId = $dbh->lastInsertId();
-if($lastInsertId)
-{
-echo '<script>alert("Your Registration successfull and your student id is  "+"'.$StudentId.'")</script>';
-}
-else 
-{
-echo "<script>alert('Something went wrong. Please try again');</script>";
-}
-    } 
-        else {    
+{   
 //Code for student ID
 $count_my_page = ("studentid.txt");
 $hits = file($count_my_page);
@@ -71,25 +36,28 @@ else
 echo "<script>alert('Something went wrong. Please try again');</script>";
 }
 }
-}
 ?>
 
-
-    <!DOCTYPE html>
-<html>
-
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Register</title>
-    <link rel="stylesheet" href="./assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
-    <link rel="stylesheet" href="./assets/fonts/fontawesome-all.min.css">
-    <link rel="stylesheet" href="./assets/fonts/font-awesome.min.css">
-    <link rel="stylesheet" href="./assets/fonts/fontawesome5-overrides.min.css">
-    <link rel="stylesheet" href="./assets/css/untitled.css">
-
-    <script type="text/javascript">
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <!--[if IE]>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <![endif]-->
+    <title>Online Library Management System | Student Signup</title>
+    <!-- BOOTSTRAP CORE STYLE  -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+    <!-- FONT AWESOME STYLE  -->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+    <!-- CUSTOM STYLE  -->
+    <link href="assets/css/style.css" rel="stylesheet" />
+    <!-- GOOGLE FONT -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+<script type="text/javascript">
 function valid()
 {
 if(document.signup.password.value!= document.signup.confirmpassword.value)
@@ -115,41 +83,71 @@ $("#loaderIcon").hide();
 error:function (){}
 });
 }
-</script> 
+</script>    
 
 </head>
+<body>
+    <!------MENU SECTION START-->
+<?php include('includes/header.php');?>
+<!-- MENU SECTION END-->
+    <div class="content-wrapper">
+         <div class="container">
+        <div class="row pad-botm">
+            <div class="col-md-12">
+                <h4 class="header-line">User Signup</h4>
+                
+                            </div>
 
-<body class="bg-gradient-primary">
-    <div class="container">
-        <div class="card shadow-lg o-hidden border-0 my-5">
-            <div class="card-body p-0">
-                <div class="p-5">
-                    <div class="text-center">
-                        <h4 class="text-dark mb-4">Create an Account!</h4>
-                    </div>
-                    <form class="user" name="signup" method="post" onSubmit="return valid();">
-                        <div class="row mb-3">
-                            <div class="col-sm-6 mb-3 mb-sm-0"><input class="form-control form-control-user" type="text" id="exampleFirstName" placeholder="Full Name" name="fullanme"></div>
-                            
+        </div>
+             <div class="row">
+           
+<div class="col-md-9 col-md-offset-1">
+               <div class="panel panel-danger">
+                        <div class="panel-heading">
+                           SINGUP FORM
                         </div>
-                        <div class="mb-3"><input class="form-control form-control-user" type="email" id="emailid" aria-describedby="emailHelp" placeholder="Email Address" name="email"></div>
-                        <div class="mb-3"><input class="form-control form-control-user" type="text" placeholder="Mobile number" name="mobileno" maxlength="10"></div>
-                        
-                        <div class="row mb-3">
-                            <div class="col-sm-6 mb-3 mb-sm-0"><input class="form-control form-control-user" type="password" placeholder="Password" name="password"></div>
-                            <div class="col-sm-6"><input class="form-control form-control-user" type="password" placeholder="Confirm Password" name="confirmpassword"></div>
-                        </div><button class="btn btn-primary d-block btn-user w-100" type="submit" id="submit"href="login.php" name="signup">Register Account</button>
-                        <hr>
-                        <hr>
-                    </form>
-                    <div class="text-center"></div>
-                    <div class="text-center"><a class="small" href="login.php">Already have an account? Login!</a></div>
-                </div>
-            </div>
+                        <div class="panel-body">
+                            <form name="signup" method="post" onSubmit="return valid();">
+<div class="form-group">
+<label>Enter Full Name</label>
+<input class="form-control" type="text" name="fullanme" autocomplete="off" required />
+</div>
+
+
+<div class="form-group">
+<label>Mobile Number :</label>
+<input class="form-control" type="text" name="mobileno" maxlength="10" autocomplete="off" required />
+</div>
+                                        
+<div class="form-group">
+<label>Enter Email</label>
+<input class="form-control" type="email" name="email" id="emailid" onBlur="checkAvailability()"  autocomplete="off" required  />
+   <span id="user-availability-status" style="font-size:12px;"></span> 
+</div>
+
+<div class="form-group">
+<label>Enter Password</label>
+<input class="form-control" type="password" name="password" autocomplete="off" required  />
+</div>
+
+<div class="form-group">
+<label>Confirm Password </label>
+<input class="form-control"  type="password" name="confirmpassword" autocomplete="off" required  />
+</div>                               
+<button type="submit" name="signup" class="btn btn-danger" id="submit">Register Now </button>
+
+                                    </form>
+                            </div>
+                        </div>
+                            </div>
         </div>
     </div>
-    <script src="./assets/bootstrap/js/bootstrap.min.js"></script>
-    <script src="./assets/js/theme.js"></script>
+    </div>
+     <!-- CONTENT-WRAPPER SECTION END-->
+    <script src="assets/js/jquery-1.10.2.js"></script>
+    <!-- BOOTSTRAP SCRIPTS  -->
+    <script src="assets/js/bootstrap.js"></script>
+      <!-- CUSTOM SCRIPTS  -->
+    <script src="assets/js/custom.js"></script>
 </body>
-
 </html>
